14ce33d08449d3c6d6362d48f43512bbe7d50fff
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-07-18 16:25:44 +0500

    eight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://horeca-seo.vercel.app"),
+  keywords: ["supply partners", "horeca", "restaurants", "bars", "hotels"],
+  title: "Supply Partners",
+  description: "Ваш деловой партнер",
+};
 
 export default function Home() {
   redirect("/ru");
