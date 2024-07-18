73c166d49ff4a38765775eed2480a96cf3726a11
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-07-18 14:37:41 +0500

    some

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+    metadataBase: new URL("https://horeca-seo.vercel.app"),
+    keywords: ["supply partners"],
+    title: {
+      default: "Supply Partners",
+      template: "%s | Supply Partners",
+    },
+    openGraph: {
+      description:
+        "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
+      images: ["https://horeca-seo.vercel.app/imgs/hero.png"],
+    },
+    description: "Ваш деловой партнер",
+  };
 
 export default function Home() {
   redirect("/ru");
