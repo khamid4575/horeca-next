677e78c8fe9fb008919c721edde62db4015eb9b6
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-07-18 12:54:24 +0500

    SEO

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,17 @@
 export default async function sitemap() {
-  return [
-    {
-      url: "https://horeca-seo.vercel.app/",
-      lastModified: new Date(),
-    },
-  ];
-}
+    return [
+      {
+        url: "https://horeca-seo.vercel.app/",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://horeca-seo.vercel.app/ru",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://horeca-seo.vercel.app/uz",
+        lastModified: new Date(),
+      },
+    ];
+  }
+  
