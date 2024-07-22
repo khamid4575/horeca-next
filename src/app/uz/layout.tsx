a39303ea2970320a40638d2bba7851df03ad92f5
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-07-22 15:47:37 +0500

    keyword

--- src/app/uz/layout.tsx
+++ src/app/uz/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import "./globals.css";
+import "../globals.css";
 import { keywords } from "@/config/keyword";
 
 // export const metadata: Metadata = {
