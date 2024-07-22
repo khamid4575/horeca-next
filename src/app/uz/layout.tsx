6f6ab934d7a2182a3a53d3444650aafba482fad3
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-07-22 16:41:18 +0500

    nine

---
+++ src/app/uz/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import "../[locale]/globals.css";
+import { keywords } from "@/config/keyword";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://horeca-seo.vercel.app"),
+  // keywords,
+  alternates: {
+    canonical: "/",
+    languages: {
+      ru: "/ru",
+      uz: "/uz",
+    },
+  },
+  title: {
+    default: "Supply Partners",
+    template: "%s | Supply Partners",
+  },
+  openGraph: {
+    description:
+      "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
+    images: ["https://horeca-seo.vercel.app/imgs/hero.png"],
+  },
+  // description: "Ваш деловой партнер lom lom lom",
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function RootLayout({
+  children,
+}: Readonly<RootLayoutProps>) {
+  const messages = await getMessages();
+  return (
+    <html lang="uz">
+      <head>
+        <link rel="icon" href="/SP.png" />
+      </head>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/uz/page.tsx
@@ -0,0 +1,57 @@
+import Contacts from "@/components/Contacts";
+import ScrollToTopIcon from "@/components/customUI/ScrollToTopIcon";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import HotelInActive from "@/components/HotelInActive";
+import HotelInConstruction from "@/components/HotelInConstruction";
+import Restaurants from "@/components/Restaurants";
+import Services from "@/components/Services";
+import Shipping from "@/components/Shipping";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://horeca-seo.vercel.app"),
+  // keywords,
+  alternates: {
+    canonical: "/",
+    languages: {
+      ru: "/ru",
+      uz: "/uz",
+    },
+  },
+  title: {
+    default: "Supply Partners",
+    template: "%s | Supply Partners",
+  },
+  openGraph: {
+    description:
+      "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
+    images: ["https://horeca-seo.vercel.app/imgs/hero.png"],
+    siteName: "Supply Partners",
+  },
+  // description: "Ваш деловой партнер lom lom lom",
+};
+
+export default function Home() {
+  return (
+    <main>
+      <link
+        href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+        rel="stylesheet"
+      />
+      <Header />
+      <Hero />
+      <div className="2xl:w-[1700px] lg:w-[1200px] mx-auto px-5 md:px-10 lg:px-0">
+        <ScrollToTopIcon />
+        <HotelInConstruction />
+        <HotelInActive />
+        <Restaurants />
+        <Services />
+        <Shipping />
+        <Contacts />
+      </div>
+      <Footer />
+    </main>
+  );
+}
