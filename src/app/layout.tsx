88a444be3983ae35f768ded8af475a5c3bfefa49
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-07-22 17:22:07 +0500

    nine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://horeca-seo.vercel.app"),
-  keywords: ["supply partners", "horeca", "restaurants", "bars", "hotels"],
-  title: "Supply Partners",
-  openGraph: {
-    description:
-      "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
-    images: ["https://horeca-seo.vercel.app/imgs/hero.png"],
-  },
-  description: "Ваш деловой партнер",
-};
+// export const metadata: Metadata = {
+//   metadataBase: new URL("https://horeca-seo.vercel.app"),
+//   keywords: ["supply partners", "horeca", "restaurants", "bars", "hotels"],
+//   title: "Supply Partners",
+//   openGraph: {
+//     description:
+//       "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
+//     images: ["https://horeca-seo.vercel.app/imgs/hero.png"],
+//   },
+//   description: "Ваш деловой партнер",
+// };
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,5 +14,6 @@ export const metadata: Metadata = {
   };
 
 export default function Home() {
-  redirect("/uz");
+  return <div>salom</div>
+  // redirect("/uz");
 }
