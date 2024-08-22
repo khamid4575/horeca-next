cd0be802e3a9b292bb3df177964b7f54753b31c1
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-08-22 16:56:59 +0500

    hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
       ></div>
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 -z-10"></div>
       <div className="flex flex-col h-[88vh] justify-center lg:items-start text-white 2xl:w-[1700px] lg:w-[1200px] mx-auto lg:text-start items-center text-center lg:px-0 px-4">
-        <h1 className="2xl:text-5xl lg:text-4xl md:text-4xl text-3xl font-bold uppercase lg:w-2/5">
+        <h1 className="2xl:text-5xl lg:text-4xl md:text-4xl text-2xl font-bold uppercase lg:w-2/5">
           {t("title")}
         </h1>
         <p className="lg:text-2xl md:text-[20px] lg:mx-0 md:w-4/5 md:mx-auto 2xl:w-[45%] lg:w-1/2 text-white font-medium my-3">
