ab07190b206c86aef4af3b76529c0d30e75d5988
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-09-19 09:30:10 +0500

    Google Analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,9 @@
+import { keywords } from "@/config/keyword";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import Script from "next/script"; // Google Analytics uchun kerak
 import "./globals.css";
-import { keywords } from "@/config/keyword";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://supplypartners.uz"),
@@ -34,14 +35,15 @@ export const metadata: Metadata = {
     title: "Supply Partners - Партнер по продуктам HORECA",
     description:
       "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
-    images: ["https://supplypartners.uz/imgs/hero.png"]
+    images: ["https://supplypartners.uz/imgs/hero.png"],
   },
   robots: {
     index: true,
     follow: true,
     noarchive: false,
   },
-  description: "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
+  description:
+    "Развивайте свой бизнес в HORECA с надежным партнером. Сделайте запрос уже сейчас и получите каталог с расценками на товары для заказа.",
 };
 
 interface RootLayoutProps {
@@ -62,7 +64,30 @@ export default async function RootLayout({
         <link rel="icon" href="/SP.png" />
         <link rel="canonical" href="https://supplypartners.uz/ru" />
         {/* <link rel="alternate" hrefLang="ru" href="https://supplypartners.uz/ru" /> */}
-        <link rel="alternate" hrefLang="uz" href="https://supplypartners.uz/uz" />
+        <link
+          rel="alternate"
+          hrefLang="uz"
+          href="https://supplypartners.uz/uz"
+        />
+        {/* Google Analytics scriptini qo'shish */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-LDE6Y3FWR0`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LDE6Y3FWR0', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body>
         <NextIntlClientProvider messages={messages}>
